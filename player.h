825e9de9aeda224b4f17ad37fac79650039ff7cb
kalandin <carlo.lucibello@gmail.com>
2014-04-17 12:58:31 +0200

    cambiato il RNG

--- player.h
+++ player.h
@@ -2,25 +2,25 @@
 #define PLAYER_H
 
 #include "game.h"
-//#include "rng.h"
 #include "rand.h"
-using namespace cpputils;
+
+
 
 class Player{
 public:
     Game * m_game;
-    UInt8 seed;
+    LinCongRNG rng;
     //ParRap rng;
 
-    Player(Game * game, UInt8 p_seed) : m_game(game), seed(p_seed){};
+    Player(Game * game, int seed = -1) : m_game(game){
+        rng.setSeed(seed);
+    };
+
     void playRandomMove(){
         const auto& moves = m_game->availMoves();
-        //auto m = moves[unifInt(rng, moves.size())];
-        
-        UInt8 r=IntRand(seed);
-        auto m = moves[r%moves.size()];
-        seed=r;
-        
+
+        auto m = moves[rng.unifInt(moves.size())];
+
         m_game->clickDiamond(m.first);
         m_game->clickDiamond(m.second);
         cout << "MOVED :  " << m.first.x() << "  " << m.first.y() << " --> " << m.second.x() << "  " << m.second.y() <<  endl;
--- rand.h
+++ rand.h
@@ -1,28 +1,62 @@
-//
-//  mt64.h
-//  Corr_Int
-//
-//  Created by jacopo rocchi on 29/11/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
 
-// queste sono le funzioni del generatore di mersenne twister 
+#include <fstream>
 
-typedef double tipo;
-typedef unsigned long long UInt8;
 
-void init_genrand64(unsigned long long seed);
-void init_by_array64(unsigned long long init_key[], unsigned long long key_length);
-unsigned long long genrand64_int64(void);
-long long genrand64_int63(void);
-double genrand64_real1(void);
-double genrand64_real2(void);
-double genrand64_real3(void);
-tipo Mersennetwister();
 
-// queste sono le funzioni del generatore alle congruenze lineari 
+class LinCongRNG {
+public:
+    typedef unsigned long long UInt8;
 
-UInt8 IntRand(UInt8 rand);
+    LinCongRNG(int seed = -1) : state(seed){
+        setSeed(seed);
+    }
+
+    UInt8 unifInt(){
+        update();
+        return state;
+    }
+
+    UInt8 unifInt(int n){
+        update();
+        return state % n;
+    }
+
+    double unifReal(){
+        update();
+        return double(state) / dmod;
+    }
+
+    void setSeed(int seed){
+        if(seed <= 0){
+            seed = randomSeed();
+        } else {
+            state = seed;
+        }
+    }
+
+private:
+    void update(){
+        state = (mult * state + add) % mod;
+    }
+
+    UInt8 randomSeed(){
+        int rdm;
+        std::ifstream urandom("/dev/urandom", ios::in|ios::binary);
+        urandom.read((char*)&rdm, 4);
+        urandom.close();
+
+        return abs(rdm) % mod;
+    }
+
+    UInt8 state;
+
+    static const UInt8 mult = 6364136223846793005ULL;
+    static const UInt8 add = UInt8(1442695040888963407ULL);
+    static const UInt8 mod = 9223372036854775807;
+    static constexpr double dmod = 9223372036854775807.;
+};
+
+
 
 
 
