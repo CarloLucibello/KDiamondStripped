0dba9e772c365e71c1adf2d970ee05e592ebce15
kalandin <carlo.lucibello@gmail.com>
2014-05-06 08:28:59 +0200

    Introdotta la possibilità di essere un Jolly nella classe diamante

--- diamond.cpp
+++ diamond.cpp
@@ -1,6 +1,10 @@
 #include "diamond.h"
 
-Diamond::Diamond(Color color) : m_color(color){}
+Diamond::Diamond(Color color)
+    : m_color(color)
+    , m_isJollyH(false)
+    , m_isJollyV(false)
+    , m_isCookie(false){}
 
 Color Diamond::color() const{
 	return m_color;
--- diamond.h
+++ diamond.h
@@ -23,8 +23,15 @@ public:
     Color color() const;
     void clicked();
     void dragged(const QPoint& direction);
+    bool isJollyH();
+    bool isJollyV();
+    bool isCookie();
+
 private:
     Color m_color;
+    bool m_isJollyV;
+    bool m_isJollyH;
+    bool m_isCookie;
 };
 
 
