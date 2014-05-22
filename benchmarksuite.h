f811b0f8184314cfb0dfb136b0fa20d0e238094a
paolobaruchspinoza <baruccap@gmail.com>
2014-05-22 18:11:25 +0200

    Update benchmarksuite.h

--- benchmarksuite.h
+++ benchmarksuite.h
@@ -23,6 +23,15 @@ public:
 
 class BenchmarkSuite{
 public:
+
+    int m_colorMin = 3;
+    int m_colorMax = 5;
+    int m_maskMin = 1;
+    int m_maskMax = 19;
+    int m_movesMin = 5;
+    int m_movesMax = 20;
+    int m_pointsMin = 60;
+    int m_pointsMax = 80;
     BenchmarkSuite(Game* game);
     BenchmarkResults testLevel(int level, double qi, int niter = 1000, int seed = -1, bool verbose = false, string paramsPath = "levels/params.txt");
     void singleGame(int level, double qi, int seedPlayer = -1, bool verbose = false);  //verbose si potrebbe eliminare da qua
@@ -31,6 +40,7 @@ public:
     void fullTestLevel( double qi, int niter, int seed, bool verbose, string paramsPath, string outPath);
 
     void testParam(string paramName, string levelPath, string expPath);
+    void fullTestParam(double qi, int niter, int seed, bool verbose);
 
 private:
     Game * m_game;
