e297fc7978870d21a6ec9d42b5fb35b70b12b31e
kalandin <carlo.lucibello@gmail.com>
2014-06-05 08:43:59 +0200

    Aggiungo i risultati dei test

---
+++ CMakeFiles/kdiamond-stripped.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/benchmarksuite.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/benchmarksuite.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/board.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/board.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/diamond.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/diamond.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/figure.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/figure.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/game.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/game.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/gameparams.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/gameparams.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/gamestate.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/gamestate.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/main.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/main.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/mask.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/mask.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/player.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/player.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/point.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/point.cpp.o"
+  "/home/carlo/Dropbox/workspace/KDiamondStripped/randomcolor.cpp" "/home/carlo/Dropbox/workspace/KDiamondStripped/CMakeFiles/kdiamond-stripped.dir/randomcolor.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/kdiamond-stripped.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/kdiamond-stripped.dir/main.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/benchmarksuite.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/gamestate.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/gameparams.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/game.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/board.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/diamond.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/randomcolor.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/mask.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/figure.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/player.cpp.o"
+  "CMakeFiles/kdiamond-stripped.dir/point.cpp.o"
+  "kdiamond-stripped.pdb"
+  "kdiamond-stripped"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/kdiamond-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
